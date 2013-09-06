2a3847af0c7706856e37800c767466c3ea035456
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-06 12:16:15 +0400

    add test for Reverse each word problem

---
+++ reverse_each_word/test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+include "test.rb"
+
+class TestReverseEachWord < Minitest::Test
+  def setup
+    @reverser = Reverser.new
+  end
+
+  def test_reversing
+    assert_equal "olleH ,ereht dna woh era ?uoy",
+	   @reverser.reverse_each_word("Hello there, and how are you?")
+  end
+end
