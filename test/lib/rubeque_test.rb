064c43c3ed51633f82a1ef60f9ef3055873c3ba2
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-09 05:52:22 +0000

    add test for Class Test problem

--- test/lib/rubeque_test.rb
+++ test/lib/rubeque_test.rb
@@ -4,10 +4,16 @@ require 'rubeque'
 class RubequeTest < Minitest::Test
   def setup
     @reverser = Reverser.new
+    @foo = ClassTest::Foo.new
+    @bar = ClassTest::Bar.new
   end
 
   def test_reversing
     assert_equal "olleH ,ereht dna woh era ?uoy",
 	   @reverser.reverse_each_word("Hello there, and how are you?")
+  end
+
+  def test_classes
+    assert_equal Bar.new.say_name, "bar"
   end
 end
