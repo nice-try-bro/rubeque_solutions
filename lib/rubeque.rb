fa7d5f13076cc69cc8f21b853c59b4d26ae80e7a
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-09 06:35:53 +0000

    put requires after module definition

--- lib/rubeque.rb
+++ lib/rubeque.rb
@@ -1,7 +1,7 @@
-require 'rubeque/version'
-require 'rubeque/class_test'
-require 'rubeque/reverse_each_word'
-
 module Rubeque
   # Your code goes here...
 end
+
+require 'rubeque/version'
+require 'rubeque/class_test'
+require 'rubeque/reverse_each_word'
