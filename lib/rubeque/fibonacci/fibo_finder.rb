2f74b10b3e6d80f5e284e2fe9094184e281dddb7
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-09 15:37:51 +0000

    pass fibonacci test

--- lib/rubeque/fibonacci/fibo_finder.rb
+++ lib/rubeque/fibonacci/fibo_finder.rb
@@ -1,8 +1,15 @@
 module Rubeque
   module Fibonacci
     class FiboFinder
-      def find(n)
+      include Math
 
+      def phi
+        (1 + sqrt(5)) / 2
+      end
+
+      def find(n)
+        #NOTE: maybe it would be better to use recursion/iteration
+        ((phi ** n - (- phi) ** (- n)) / (2 * phi - 1)).round(0)
       end
     end
   end
