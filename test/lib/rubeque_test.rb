f1af10b707a9224fab7ab418b0006fd87ee45d7d
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-10 07:10:35 +0000

    inmplement random generator test

--- test/lib/rubeque_test.rb
+++ test/lib/rubeque_test.rb
@@ -6,6 +6,7 @@ class RubequeTest < TestCase
     @foo = Rubeque::ClassTest::Foo.new
     @bar = Rubeque::ClassTest::Bar.new
     @fib_generator = Rubeque::Fibonacci::FiboFinder.new
+    @rand_generator = Rubeque::Randomness::RandGenerator.new
   end
 
   def test_reversing
@@ -23,5 +24,17 @@ class RubequeTest < TestCase
     assert_equal 3, @fib_generator.find(4)
     assert_equal 13, @fib_generator.find(7)
     assert_equal 55, @fib_generator.find(10)
+  end
+
+  def test_random_generator
+    #NOTE: maybe those aquirings shouldn't be there
+    a = @rand_generator.generate
+    b = @rand_generator.generate
+    c = @rand_generator.generate
+    d = @rand_generator.generate
+    assert_equal true, (a > 99 && a < 501)
+    assert_equal true, (b > 99 && b < 501)
+    assert_equal true, (c > 99 && c < 501)
+    assert_equal true, (d > 99 && d < 501)
   end
 end
