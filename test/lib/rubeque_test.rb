2d4e7bb05dfc93413c881f81c12b9fd23ee02b13
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-10 12:15:56 +0000

    implement regex test

--- test/lib/rubeque_test.rb
+++ test/lib/rubeque_test.rb
@@ -8,6 +8,7 @@ class RubequeTest < TestCase
     @fib_generator = Rubeque::Fibonacci::FiboFinder.new
     @rand_generator = Rubeque::Randomness::RandGenerator.new
     @hash_min_finder = Rubeque::HashLittleValue::HashMinFinder.new
+    @regex_applicator = Rubeque::RegularExpressions::RegExApplicator.new
   end
 
   def test_reversing
@@ -43,5 +44,11 @@ class RubequeTest < TestCase
     assert_equal @hash_min_finder.key_for_min_value({"k" => 2, "h" => 3, "j" => 1}), "j"
     assert_equal @hash_min_finder.key_for_min_value({"o" => 0, "z" => -2, "j" => 1}), "z"
     assert_equal @hash_min_finder.key_for_min_value({}), nil
+  end
+
+  def test_regex
+    assert_equal ["john@mail.com", "jane@mail.com"], @regex_applicator.apply_regex("john@mail.com to jane@mail.com")
+    assert_equal ["why@chunkybacon.com"], @regex_applicator.apply_regex("why@ @chunkybacon.com why@chunkybacon.com")
+    assert_equal [], @regex_applicator.apply_regex("why @ @ why@chunkybacon")
   end
 end
