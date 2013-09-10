94a1407e44e5014c9ac29938e4afba82736cc6d6
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-10 14:41:44 +0000

    fixed argument order of asserts in hash test

--- test/lib/rubeque_test.rb
+++ test/lib/rubeque_test.rb
@@ -41,9 +41,9 @@ class RubequeTest < TestCase
   end
 
   def test_hash_little_value
-    assert_equal @hash_min_finder.key_for_min_value({"k" => 2, "h" => 3, "j" => 1}), "j"
-    assert_equal @hash_min_finder.key_for_min_value({"o" => 0, "z" => -2, "j" => 1}), "z"
-    assert_equal @hash_min_finder.key_for_min_value({}), nil
+    assert_equal "j", @hash_min_finder.key_for_min_value({"k" => 2, "h" => 3, "j" => 1})
+    assert_equal "z", @hash_min_finder.key_for_min_value({"o" => 0, "z" => -2, "j" => 1})
+    assert_equal nil, @hash_min_finder.key_for_min_value({})
   end
 
   def test_regex
