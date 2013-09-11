0e2ef21c697a87490454220ff54e11aa4f313508
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-11 16:55:57 +0000

    add coveralls call to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 require 'bundler/setup'
 Bundler.require
+
 require 'minitest/autorun'
+
+
+if ENV['TRAVIS']
+    require 'coveralls'
+      Coveralls.wear!
+end
 
 class TestCase < MiniTest::Test
 end
