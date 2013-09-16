9b4df731ae9ff4267cb1332a8260438bffdc4149
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-16 18:42:28 +0000

    add rectangle creation test

--- test/lib/rectangle_area_test.rb
+++ test/lib/rectangle_area_test.rb
@@ -6,4 +6,12 @@ class RectangleAreaTestCase < TestCase
     assert_equal 1, point.x
     assert_equal 2, point.y
   end
+
+  def test_rectangle_creation
+    p1 = Rubeque::RectangleArea::Point.new(0, 0)
+    p2 = Rubeque::RectangleArea::Point.new(3, 4)
+    rectangle = Rubeque::RectangleArea::Rectangle.new
+    accert_equal p1, rectangle.point1
+    accert_equal p2, rectangle.point2
+  end
 end
