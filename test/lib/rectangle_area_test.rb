a4c02d603d111c62812cc905eaae508224aa406c
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-16 10:33:56 +0000

    fix point test

--- test/lib/rectangle_area_test.rb
+++ test/lib/rectangle_area_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class RectangleAreaTestCase < TestCase
-  def testPointCreation
-    point = Point.new 1, 2
+  def test_point_creation
+    point = Rubeque::RectangleArea::Point.new(1, 2)
     assert_equal 1, point.x
     assert_equal 2, point.y
   end
