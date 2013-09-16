b9c54c3071f6d93c65ad66cfe13b1e01848fc728
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-16 09:07:46 +0000

    fix method acting test

--- test/lib/method_acting_test.rb
+++ test/lib/method_acting_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MethodActingTestCase < TestCase
   def test_method_names
-    method_acter = Rubeque::MethodActing::MethodActor.new
+    method_acter = Rubeque::MethodActing::MethodActer.new
     assert_equal :dogs. method_acter.dogs
     assert_equal :cats. method_acter.cats
     assert_equal :chunky_bacon. method_acter.chunky_bacon
