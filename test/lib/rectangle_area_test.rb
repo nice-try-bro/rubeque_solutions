be396ac0e8faea02ce6db6ab38ff3a0a76c574cb
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-17 08:21:22 +0000

    refactor rectangle area test

--- test/lib/rectangle_area_test.rb
+++ test/lib/rectangle_area_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
 class RectangleAreaTestCase < TestCase
+  class ObjectCreator
+    def createRectangle(x1, y1, x2, y2)
+      point1 = Rubeque::RectangleArea::Point.new x1, y1
+      point2 = Rubeque::RectangleArea::Point.new x2, y2
+      Rubeque::RectangleArea::Rectangle.new point1, point2
+    end
+  end
+
+  def setup
+    @creator = ObjectCreator.new
+  end
+
   def test_point_creation
     point = Rubeque::RectangleArea::Point.new 1, 2
     assert_equal 1, point.x
@@ -8,18 +20,15 @@ class RectangleAreaTestCase < TestCase
   end
 
   def test_rectangle_creation
-    point_1 = Rubeque::RectangleArea::Point.new 0, 0
-    point_2 = Rubeque::RectangleArea::Point.new 3, 4
-    rectangle = Rubeque::RectangleArea::Rectangle.new point_1, point_2
-    assert_equal point_1, rectangle.point1
-    assert_equal point_2, rectangle.point2
+    rectangle = @creator.createRectangle 0, 2, 3, 4
+    assert_equal 0, rectangle.point1.x
+    assert_equal 2, rectangle.point1.y
+    assert_equal 3, rectangle.point2.x
+    assert_equal 4, rectangle.point2.y
   end
 
   def test_rectangle_area
-    point_1 = Rubeque::RectangleArea::Point.new 0, 0
-    point_2 = Rubeque::RectangleArea::Point.new 3, 4
-    rectangle = Rubeque::RectangleArea::Rectangle.new point_1, point_2
-    expected_area = (point_2.x - point_1.x) * (point_2.y - point_1.y)
-    assert_equal expected_area, rectangle.area
+    rectangle = @creator.createRectangle 2, 4, 7, -3
+    assert_equal 20, rectangle.area
   end
 end
