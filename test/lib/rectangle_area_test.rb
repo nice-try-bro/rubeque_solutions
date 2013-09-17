3d9e3e493e54bfb55a04cbd3652e99d155a30fe1
Alexey Kuznetsov <innercloister@gmail.com>
2013-09-17 07:42:57 +0000

    add rectangle area test

--- test/lib/rectangle_area_test.rb
+++ test/lib/rectangle_area_test.rb
@@ -14,4 +14,12 @@ class RectangleAreaTestCase < TestCase
     assert_equal point_1, rectangle.point1
     assert_equal point_2, rectangle.point2
   end
+
+  def test_rectangle_area
+    point_1 = Rubeque::RectangleArea::Point.new 0, 0
+    point_2 = Rubeque::RectangleArea::Point.new 3, 4
+    rectangle = Rubeque::RectangleArea::Rectangle.new point_1, point_2
+    expected_area = (point_2.x - point_1.x) * (point_2.y - point_1.y)
+    assert_equal expected_area, rectangle.area
+  end
 end
